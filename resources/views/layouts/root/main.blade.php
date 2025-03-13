1f5e73b181e35e90ab320f3e31222d2341db036a
AIIA\fabian <mitsalf1@gmail.com>
2025-03-13 14:48:03 +0700

    disable

--- resources/views/layouts/root/main.blade.php
+++ resources/views/layouts/root/main.blade.php
@@ -31,13 +31,9 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
     <!--begin::Fonts(mandatory for all pages)-->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" /> <!--end::Fonts-->
+    {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" /> <!--end::Fonts--> --}}
 
     <!--begin::Vendor Stylesheets(used for this page only)-->
-    <link href="{{ asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.css') }}" rel="stylesheet"
-        type="text/css" />
-    <link href="{{ asset('assets/plugins/custom/datatables/datatables.bundle.css') }}" rel="stylesheet"
-        type="text/css" />
     <!--end::Vendor Stylesheets-->
 
     <!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
@@ -47,10 +43,6 @@
     <!--end::Global Stylesheets Bundle-->
 
     @stack('custom-css')
-
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-annotation"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script>
 
 </head>
 <!--end::Head-->
