98c4dee365c70fedb79c135fe574666625a9b558
AIIA\fabian <mitsalf1@gmail.com>
2025-04-08 14:08:47 +0700

    update page

--- resources/views/website/dashboard/index.blade.php
+++ resources/views/website/dashboard/index.blade.php
@@ -10,7 +10,9 @@
 
 
 @section('main')
-    <div id="kt_app_content_container" class="app-container  container-fluid ">
-        <img src="{{ asset('assets/media/website/dashboard.jpeg') }}" alt="" width="1100px">
+    <div id="kt_app_content_container" class="app-container  container-fluid text-center">
+        <img src="{{ asset('assets/media/website/dashboard.jpeg') }}" alt="" width="1100px" class="mb-8">
+        <img src="{{ asset('assets/media/website/master.jpeg') }}" alt="" width="1100px" class="mb-8">
+        <img src="{{ asset('assets/media/website/people.png') }}" alt="" width="900px" class="mb-8">
     </div>
 @endsection
