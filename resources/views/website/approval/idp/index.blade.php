eeb139f66cde674f42a353acd3f6f2305b728179
AIIA\fabian <mitsalf1@gmail.com>
2025-07-07 15:09:14 +0700

    revise IDP

--- resources/views/website/approval/idp/index.blade.php
+++ resources/views/website/approval/idp/index.blade.php
@@ -86,8 +86,8 @@
                                                     <div class="table-responsive">
                                                         <table class="table table-bordered table-sm">
                                                             <thead class="table-light">
-                                                                <tr>
-                                                                    <th class="text-left p-2">ALC</th>
+                                                                <tr class="p-2">
+                                                                    <th class="text-left">ALC</th>
                                                                     <th class="text-center">Category</th>
                                                                     <th>Program</th>
                                                                     <th>Target</th>
