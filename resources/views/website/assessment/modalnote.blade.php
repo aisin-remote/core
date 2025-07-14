f92a62721cf082028d44443ea13b2cbc910bd924
NaufalPujiMahdy <naufalpm230800@gmail.com>
2025-07-14 15:14:25 +0700

    feat: add modal for assessment notes

---
+++ resources/views/website/assessment/modalnote.blade.php
@@ -0,0 +1,22 @@
+<div class="modal fade" id="noteAssessmentModal" tabindex="-1" aria-labelledby="noteAssessmentModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="noteAssessmentModalLabel">Note Assessment</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form id="noteAssessmentForm">
+                    <div class="mb-3">
+                        <label for="assessmentNote" class="form-label">Assessment Note</label>
+                        <textarea class="form-control" id="assessmentNote" name="assessmentNote" rows="4" readonly></textarea>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
