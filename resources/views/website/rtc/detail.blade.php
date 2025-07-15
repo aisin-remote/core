a422d8fec2645c9e4f0431a6f0bdcde7bc24357b
NaufalPujiMahdy <naufalpm230800@gmail.com>
2025-07-15 09:18:34 +0700

    feat: implement organizational chart for RTC detail view; integrate OrgChart library for visual representation of managers and supervisors

--- resources/views/website/rtc/detail.blade.php
+++ resources/views/website/rtc/detail.blade.php
@@ -4,52 +4,69 @@
 @section('breadcrumbs', $title ?? 'RTC')
 
 @section('main')
-    <div class="rtc-container">
-
-        @include('components.rtc-card', [
-            'title' => $main['title'],
-            'person' => $main['person'],
-            'shortTerm' => $main['shortTerm'],
-            'midTerm' => $main['midTerm'],
-            'longTerm' => $main['longTerm'],
-            'cardClass' => 'rtc-main-card',
-        ])
-
-        {{-- Display managers and supervisors --}}
-        <div class="rtc-level-container">
-            @foreach ($managers as $manager)
-                @include('components.rtc-card', [
-                    'title' => $manager['title'] ?? '',
-                    'person' => $manager['person'] ?? '',
-                    'shortTerm' => $manager['shortTerm'] ?? '',
-                    'midTerm' => $manager['midTerm'] ?? '',
-                    'longTerm' => $manager['longTerm'] ?? '',
-                    'cardClass' => "rtc-manager-card {$manager['colorClass']}",
-                ])
-            @endforeach
-        </div>
-
-        <div class="rtc-supervisor-container">
-            @foreach ($managers as $manager)
-                @if (!empty($manager['supervisors']))
-                    <div class="rtc-supervisor-group">
-                        @foreach ($manager['supervisors'] as $spv)
-                            @include('components.rtc-card', [
-                                'title' => $spv['title'],
-                                'person' => $spv['person'],
-                                'shortTerm' => $spv['shortTerm'],
-                                'midTerm' => $spv['midTerm'],
-                                'longTerm' => $spv['longTerm'],
-                                'cardClass' => "rtc-supervisor-card {$spv['colorClass']}",
-                            ])
-                        @endforeach
-                    </div>
-                @endif
-            @endforeach
-        </div>
-
-    </div>
-
+    <div id = "orgchart-container" style = "width:100%; height: 700px;"></div>
     {{-- Styles tetap di sini seperti sebelumnya --}}
     @include('website.rtc.style.index')
 @endsection
+
+@push('scripts')
+    <script src="https://balkan.app/js/OrgChart.js"></script>
+
+    <script>
+        const main = @json($main);
+        const managers = @json($managers);
+    </script>
+
+    <script>
+        function buildChartData(main, managers) {
+            let nodes = [];
+            console.log(main);
+
+
+            // Main person (GM)
+            const rootId = 'root';
+            nodes.push({
+                id: rootId,
+                name: main.person.name,
+                title: main.person.grade,
+                grade: `Age: ${main.person.age ?? '-'} | LOS: ${main.person.los ?? '-'}`,
+            });
+
+            // Managers
+            managers.forEach((manager, i) => {
+                const managerId = `m-${i}`;
+                nodes.push({
+                    id: managerId,
+                    pid: rootId,
+                    name: manager.person.name,
+                    title: manager.person.grade,
+                    grade: `Age: ${manager.person.age ?? '-'} | LOS: ${manager.person.los ?? '-'}`,
+                });
+
+                // Supervisors
+                (manager.supervisors ?? []).forEach((spv, j) => {
+                    const spvId = `m-${i}-s-${j}`;
+                    nodes.push({
+                        id: spvId,
+                        pid: managerId,
+                        name: spv.person.name,
+                        title: spv.person.grade,
+                        grade: `Age: ${spv.person.age ?? '-'} | LOS: ${spv.person.los ?? '-'}`,
+                    });
+                });
+            });
+
+            return nodes;
+        }
+
+        const chart = new OrgChart(document.getElementById("orgchart-container"), {
+            template: "ana",
+            nodeBinding: {
+                field_0: "name",
+                field_1: "title",
+                field_2: "grade",
+            },
+            nodes: buildChartData(main, managers)
+        });
+    </script>
+@endpush
