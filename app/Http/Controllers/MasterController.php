66868d7f55765db35e2404602d7d2b6197464603
NaufalPujiMahdy <naufalpm230800@gmail.com>
2025-07-31 14:25:48 +0700

    feat: make datatables to master department

--- app/Http/Controllers/MasterController.php
+++ app/Http/Controllers/MasterController.php
@@ -357,7 +357,7 @@ class MasterController extends Controller
                 $q->whereHas('manager', function ($sub) use ($company) {
                     $sub->where('company_name', $company);
                 });
-            })->paginate(10);
+            })->get();
 
         $managers = Employee::when($company, function ($q) use ($company) {
             $q->where('company_name', $company);
